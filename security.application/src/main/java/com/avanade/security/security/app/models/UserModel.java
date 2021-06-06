5c07550cfd4cca91fad7c64f1b817c824290e5ca
mspontes360 <mspontes360@gmail.com>
2021-06-05 22:47:06 -0300

    Developed UserModel and add spring-boot-starter-data-jpa dependency to pom.xml

---
+++ security.application/src/main/java/com/avanade/security/security/app/models/UserModel.java
@@ -0,0 +1,47 @@
+package com.avanade.security.security.app.models;
+
+import javax.persistence.Entity;
+
+@Entity
+public class UserModel {
+
+    private String UserName;
+
+    private String password;
+
+    private String firstName;
+
+    private String lastName;
+
+    public String getUserName() {
+        return UserName;
+    }
+
+    public void setUserName(String userName) {
+        UserName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
